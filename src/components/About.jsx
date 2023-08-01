6eabec8de8ceb4ea855779a07b662fb54f6cca5c
JackScottow <j.scottow@googlemail.com>
2023-08-01 19:48:21 +0100

    typo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className="lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
           <div>
             <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">About Me</h2>
-            <p className="max-w-3xl mt-3 text-lg text-gray-300">Hi there! I&apos;m Jack, a front-end developer based near London specialising in React. I have expertise in a wide range of web technologies and I&apos;m passionate about creating beautiful and functional websites. Whether you&apos;re looking for a website revamp or a brand new build, I offer the necessary skills to meet your needs. Feel free to check out my some of my work!</p>
+            <p className="max-w-3xl mt-3 text-lg text-gray-300">Hi there! I&apos;m Jack, a front-end developer based near London specialising in React. I have expertise in a wide range of web technologies and I&apos;m passionate about creating beautiful and functional websites. Whether you&apos;re looking for a website revamp or a brand new build, I offer the necessary skills to meet your needs. Feel free to check out some of my work!</p>
           </div>
           <div className="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
             <div className="flex justify-center col-span-1 px-8 py-8 ">
