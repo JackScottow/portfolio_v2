7b7b9563346c1ea2e67fa04072f8ee67c8fd3ba3
JackScottow <101928816+JackScottow@users.noreply.github.com>
2023-08-02 19:41:33 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact, faSquareJs, faNodeJs, faCss3, faHtml5, faShopify } from "@fortawesome/free-brands-svg-icons";
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
 import { Tooltip, Zoom } from "@mui/material";
+import Link from "next/link";
 
 const About = () => {
   return (
@@ -9,9 +11,17 @@ const About = () => {
         <div className="lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
           <div>
             <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">About Me</h2>
-            <p className="max-w-3xl mt-3 text-lg text-gray-300">Hi there! I&apos;m Jack, a front-end developer based near London specialising in React. I have expertise in a wide range of web technologies and I&apos;m passionate about creating beautiful and functional websites. Whether you&apos;re looking for a website revamp or a brand new build, I offer the necessary skills to meet your needs. Feel free to check out my some of my work!</p>
+            <p className="max-w-3xl mt-3 text-lg text-gray-300">Hi there! I&apos;m Jack, a front-end developer based near London specialising in React. I have expertise in a wide range of web technologies and I&apos;m passionate about creating beautiful and functional websites. Feel free to check out some of my work! </p>
+            <ul className="grid grid-cols-2 pl-5 mt-5 text-justify list-disc gap-y-2">
+              <li>HTML/CSS/JS</li>
+              <li>React/NextJS</li>
+              <li>SASS</li>
+              <li>NodeJS</li>
+              <li>Git</li>
+              <li>Shopify</li>
+            </ul>
           </div>
-          <div className="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
+          <div className=" grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
             <div className="flex justify-center col-span-1 px-8 py-8 ">
               <Tooltip title="React" placement="top" disableInteractive TransitionComponent={Zoom} arrow enterTouchDelay={0}>
                 <FontAwesomeIcon icon={faReact} size="4x" />
