d4ae4aa09bb0e1e6579a2466ded84202cffb4364
JackScottow <101928816+JackScottow@users.noreply.github.com>
2025-02-22 15:33:57 +0000

    blurb

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -20,7 +20,7 @@ const Landing = () => {
             <Typewriter words={["", "Jack Scottow"]} typeSpeed={80} delaySpeed={1500} />
           </motion.h2>
           <motion.p initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.6 }} className="py-4 text-xl leading-relaxed text-gray-400 sm:text-2xl">
-            <Typewriter words={["", "I'm a front-end web developer who is passionate about creating beautifully functional websites"]} typeSpeed={20} delaySpeed={3000} />
+            <Typewriter words={["", "I'm a full-stack web developer passionate about building beautifully functional websites and robust, scalable applications. From sleek front-end designs to powerful back-end solutions, I craft seamless digital experiences that bring ideas to life"]} typeSpeed={20} delaySpeed={3000} />
           </motion.p>
         </motion.div>
       </header>
